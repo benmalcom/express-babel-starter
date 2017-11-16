48227afc0ac74be930e7ce80bf1c5d216a311ef7
Benjamin Ikeji <benjaminikeji@gmail.com>
2017-11-16 01:03:35 +0100

    Fresh cup

--- config/default.js
+++ config/default.js
@@ -3,29 +3,8 @@ const PORT = process.env.PORT || 3000;
 module.exports = {
 	app:
     {
-    	name: 'todo-api',
+    	name: 'express-babel-starter',
     	port: PORT,
     	baseUrl: `http://localhost:${PORT}`,
-    },
-	api:
-    {
-    	prefix: '/api',
-    	versions: [1],
-    },
-	lang: 'en',
-	authToken: {
-		superSecret: 'ipa-odot',
-		expiresIn: 86400,
-	},
-	db: {
-		url: 'mongodb://localhost:27017/todo',
-	},
-	/* twilio:{
-        ACCOUNT_SID: "ACe434eabecbe0aad4562b2f65bac61e3e",
-        AUTH_TOKEN: "e643b2d691c0606a1c40f0a3c7a75009",
-        FROM: ""
-    },*/
-	itemsPerPage: {
-		default: 10,
-	},
+    }
 };
